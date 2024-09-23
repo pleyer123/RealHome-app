6c134eb44192a0bfa9124ab5f074afb13d7bc19c
pleyer123 <chrissiejohnson73@gmail.com>
2024-09-23 12:45:53 +0200

    Changes to hero small mobile

--- src/HeroSection/Hero.jsx
+++ src/HeroSection/Hero.jsx
@@ -5,7 +5,9 @@ import "./Hero.css"
 
 function Hero(){
 
-
+    const propertySection = () => {
+        window.open("/Listings","_blank")
+    }
     const [isMobile, setIsMobile] = useState(false)
     const [isLogin, setIslogin] = useState(false)
 
@@ -16,13 +18,15 @@ function Hero(){
         <div className="hero-container">
 
             <nav className="navbar">
+                <img src="./RealHomesWHITE.png" alt="" />
                 <ul className={isMobile ? "nav-links-mobile" : "nav-links"} onClick={() => setIsMobile(false)}>
-                    <img src="app.logomakr.com/1JCVAQ" alt="" />
                     <li><a href="#">Home</a></li>
                     <li><a href="#fprop-container">Proporty Listings</a></li>
                     <li><a href="#Agents">Agents</a></li>
                     <li><a href="#about-us">About Us</a></li>
                     <li><a href="#contactUs">Contact Us</a></li>
+                    <li><a className="login-signup" href="#Login">Login</a></li>
+                    <li><a className="login-signup" href="#Sign-Up">Sign-Up</a></li>
                 </ul>
                 <button className="mobile-menu-icon" onClick={() => setIsMobile(!isMobile)} >
                 {isMobile ? <i className="fas fa-times"></i> : <i className="fas fa-bars"></i>}
@@ -35,7 +39,7 @@ function Hero(){
             
             <h1 className="heading">Welcome To RealHome</h1>
             <h2 className="sub-heading">Find Your Dream Home With Us</h2>
-            <button className="cta-button">Get Started</button>
+            <button className="cta-button" onClick={propertySection}>Get Started</button>
 
 
             <div className="background">
