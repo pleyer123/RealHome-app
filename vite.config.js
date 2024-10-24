bdedf2b558eb5ca20e0c104fb7ced2c59c368156
Christopher <chrissiejohnson73@gmail.com>
2024-10-24 07:56:01 +0200

    updated build settings

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@supabase/supabase-js'],
+    },
+  },
   server: {
     port: 5173, // Ensure this matches your setup
     cors: true, // Enable CORS if necessary
