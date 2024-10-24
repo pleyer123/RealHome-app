9846cee695b89762947287be8f1d75f73d7a54b5
Christopher <chrissiejohnson73@gmail.com>
2024-10-24 09:23:04 +0200

    main changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,14 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx';
 import './HeroSection/Hero.css';
 
+console.log(window.location.origin)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Auth0Provider
       domain="dev-qz7a5av7sksmr4qs.us.auth0.com"
       clientId="ua319xDQDqXc0aiJXQjdAyYw2wFmO20A"
-      authorizationParams={{
-        redirect_uri:"http://localhost:5173"
-      }}
-    scope='openid profile email'
+     redirectUri={window.location.origin}
     >
       <App />
     </Auth0Provider>
