dbbd488ecc2f405156489cba9faf8e1016cbd391
Christopher <chrissiejohnson73@gmail.com>
2024-10-28 13:00:21 +0200

    updates to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['@supabase/supabase-js'],
-    },
+    // No need to externalize @supabase/supabase-js in Vite config
   },
   server: {
     port: 5173,
